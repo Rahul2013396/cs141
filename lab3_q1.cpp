648e7f2d3c42cceb11d5200aabfd669291ab7b33
Rahul2013396 <42237507+Rahul2013396@users.noreply.github.com>
2018-08-17 10:54:53 +0530

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,18 @@
+// first include the library
+#include <iostream>
+using namespace std;
+// write the function
+int main(){
+// declaration of data types
+int a;
+float b;
+char c;
+bool d;
+double e;
+//print the results
+cout << " size of int is "<< sizeof (a) <<endl;
+cout << " size of float is "<< sizeof (b) <<endl;
+cout << " size of char is "<< sizeof (c) <<endl;
+cout << " size of bool is "<< sizeof (d) <<endl;
+cout << " size of double is "<< sizeof (e) <<endl;
+}
---
+++ lab3_q2.cpp
@@ -0,0 +1,24 @@
+// include the library
+#include <iostream>
+using namespace std;
+// write the function
+int main(){
+// declare the variables
+int a;
+float b;
+int c;
+// assigning the values
+a=3;
+b=4.5;
+c=2;
+
+// printing the result
+cout << "the sum is " <<a+b <<endl;
+cout << "the difference is "<< a-b << endl;
+cout <<  "the product is " << a*b <<endl;
+cout << "the quotient is " << a/b <<endl;
+cout << "the modulus is " << a%c << endl;
+}
+
+
+
