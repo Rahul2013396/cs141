25f89f5e41ce2250ffa3b30f8259bd6445815da5
Rahul2013396 <42237507+Rahul2013396@users.noreply.github.com>
2018-08-22 14:08:14 +0530

    Add files via upload

---
+++ lab4_q4.cpp
@@ -0,0 +1,13 @@
+//include library
+#include <iostream>
+using namespace std;
+// write the function
+int main(){
+//declaring variables
+float days;
+cout <<" number of days? ";
+cin >>days;
+// defining year and week in from of days and obtaining output
+cout <<" the year to "<< days/365 << " year "<<endl;
+cout <<" the week to "<< days/52 << " week "<<endl;
+}
