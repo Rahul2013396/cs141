18b1efcc0761dee9c437137ae20e679cbd03fca7
Rahul2013396 <42237507+Rahul2013396@users.noreply.github.com>
2018-08-22 14:31:04 +0530

    Update lab4_q4.cpp

--- lab4_q4.cpp
+++ lab4_q4.cpp
@@ -4,10 +4,14 @@ using namespace std;
 // write the function
 int main(){
 //declaring variables
-float days;
-cout <<" number of days? ";
-cin >>days;
-// defining year and week in from of days and obtaining output
-cout <<" the year to "<< days/365 << " year "<<endl;
-cout <<" the week to "<< days/52 << " week "<<endl;
-}
+int days , years , weeks , daysleft1 , daysleft2;
+cout <<  "how many days u want to convert " ;
+cin >> days;
+// defining variables
+years = days/365;
+daysleft1 = days%365;
+weeks = daysleft1/7;
+daysleft2 = weeks%7;
+//output
+cout << days << " is equal to "<< years << " years , "<< weeks << " weeks and " << daysleft2 << " days";
+}  
